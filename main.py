b86782344169a60f91e2c9bfed0fa053e62671a1
Itaybl14 <84288727+Itaybl14@users.noreply.github.com>
2022-07-05 02:45:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ with open("token.txt") as f:
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.66 Safari/537.36 Edg/103.0.1264.44","Authorization":token,"Ubi-AppId":appid,"Content-Type":"application/json"}
 r = requests.post('https://public-ubiservices.ubi.com/v3/profiles/sessions', headers=headers)
-if r.json()["ticket"]:
+if "ticket" in r.json():
     ticket = r.json()["ticket"]
 else:
     import time
